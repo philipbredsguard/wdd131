888ff48554a6a83bede52a2890ef0edb14738cc2
philipbredsguard <philip.bredsguard@gmail.com>
2026-03-13 07:39:45 -0400

    done

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,20 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('_______')
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = 'X';
-li.append(deleteButton);
-li.append(li);
+const list = document.querySelector('#list')
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'X';
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        li.appendChild(deleteBtn);
+        listElement.appendChild(li);
+        inputElement.value = "";
+    }
+    inputElement.focus();
+});
