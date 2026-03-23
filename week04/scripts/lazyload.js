2ae0013905566478ccab07e096538ade3daf55dd
philipbredsguard <philip.bredsguard@gmail.com>
2026-03-23 10:29:10 -0400

    .

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,5 @@
+// Set current year in footer
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Display the last modified date of the document
+document.getElementById('lastModified').textContent = document.lastModified;
